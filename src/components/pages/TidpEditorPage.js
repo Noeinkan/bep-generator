b989ebcbc6703cc5ba34246c71595f9cce897efa
Noeinkan <andrea.aita@libero.it>
2025-09-30 01:47:16 +0100

    ok TIDP funziona meglio

---
+++ src/components/pages/TidpEditorPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TidpMidpManager from './TidpMidpManager';
+
+const TidpEditorPage = () => {
+  return <TidpMidpManager initialShowTidpForm={true} />;
+};
+
+export default TidpEditorPage;
